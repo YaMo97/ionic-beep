f5fd6a7530cfcff2b61db3abc4495c952a8bf4eb
Yatharth Manocha <yathman97@gmail.com>
2018-03-29 20:16:58 +0530

    Add avatar.png to profiles

--- src/mocks/profiles/profiles.ts
+++ src/mocks/profiles/profiles.ts
@@ -4,28 +4,28 @@ const userList: Profile[] = [
     {
         firstName: 'Yatharth',
         lastName: 'Manocha',
-        avatar: 'assets/imgs/logo.png',
+        avatar: 'assets/imgs/avatar.png',
         email: 'yathman97@gmail.com',
         dateOfBirth: new Date()
     },
     {
         firstName: 'Anit',
         lastName: 'Singh',
-        avatar: 'assets/imgs/logo.png',
+        avatar: 'assets/imgs/avatar.png',
         email: 'anit@gmail.com',
         dateOfBirth: new Date()
     },
     {
         firstName: 'Siddharth',
         lastName: 'Manocha',
-        avatar: 'assets/imgs/logo.png',
+        avatar: 'assets/imgs/avatar.png',
         email: 'siddharth@gmail.com',
         dateOfBirth: new Date()
     },
     {
         firstName: 'Udit',
         lastName: 'Nagpal',
-        avatar: 'assets/imgs/logo.png',
+        avatar: 'assets/imgs/avatar.png',
         email: 'udit@gmail.com',
         dateOfBirth: new Date()
     }
