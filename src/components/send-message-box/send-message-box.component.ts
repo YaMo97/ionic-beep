653fb01122f7040d91e409776538a50c45b94bb2
Yatharth Manocha <yathman97@gmail.com>
2018-03-30 16:14:29 +0530

    Add Send Message Box Component

---
+++ src/components/send-message-box/send-message-box.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+/**
+ * Generated class for the SendMessageBoxComponent component.
+ *
+ * See https://angular.io/api/core/Component for more info on Angular
+ * Components.
+ */
+@Component({
+  selector: 'app-send-message-box',
+  templateUrl: 'send-message-box.component.html'
+})
+export class SendMessageBoxComponent {
+
+  constructor() {
+  }
+
+}
